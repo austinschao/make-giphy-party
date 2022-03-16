10aeb478a181d80fb42f21b7e4540ce2bc76eb89
Robyn Lam <robynlamgy@gmail.com>
2022-03-15 17:27:26 -0700

    completed with review

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 "use strict";
 
 const $searchBtn = $('#search-gif');
-$searchBtn.on('click', getSearchVal);
-
 const $gifParty = $('#gif-party');
+const $removeBtn = $('#remove-gif');
+
+$searchBtn.on('click', getSearchVal);
+$removeBtn.on('click', removeGif);
 
 /** Make random number */
 
-function makeRandNum () {
+function makeRandNum() {
   return Math.floor(Math.random() * 50);
 }
 
 /** Make an image */
 
-function makeImg(url) {
+function addImg(url) {
   const $img = $('<img>');
   $img.attr('src', url);
   $gifParty.append($img);
 }
-
 
 /** Append the gif to the DIV */
 
@@ -26,23 +27,24 @@ function addGifs(res) {
   const randomGifNum = makeRandNum();
   const imgUrl = res.data.data[randomGifNum].images.original.url;
 
-  makeImg(imgUrl);
+  addImg(imgUrl);
 }
 
-/** Retrieving input from search bar and
- *   sends HTTP request
-*/
+/**Remove all elements inside of gifParty */
+function removeGif(e) {
+  e.preventDefault();
+  $gifParty.empty();
+}
+
+/** Retrieving input from search bar and sends HTTP request */
 
 async function getSearchVal(e) {
   e.preventDefault();
   let searchTerm = $('#search-term').val();
-  let response = await axios.get('http://api.giphy.com/v1/gifs/search', {params:{q: searchTerm, api_key: 'q7MxlJQCOVBgdiLRTBj9yGQcO5v4O8pD'}});
-  console.log(response, 'response')
+  $searchInput.val("");
+
+  let response = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { q: searchTerm, api_key: 'q7MxlJQCOVBgdiLRTBj9yGQcO5v4O8pD' } });
+  console.log(response, 'response');
   addGifs(response);
+
 }
-
-
-
-
-
-console.log("Let's get this party started!");
